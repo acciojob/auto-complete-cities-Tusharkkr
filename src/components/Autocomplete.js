c30e2c7da0b285d133c45d9e406b9e5112eaca75
Tushar kumar <narutoboi41@gmail.com>
2026-04-16 13:06:25 +0530

    done

---
+++ src/components/Autocomplete.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const cities =
+    [
+        "Delhi", "Mumbai", "Bangalore", "Hyderabad", "Chennai", "Kolkata", "Pune", "Ahmedabad", "Jaipur", "Lucknow", "Kanpur", "Nagpur", "Indore", "Bhopal", "Patna", "Surat", "Vadodara", "Ludhiana", "Agra", "Varanasi", "Meerut", "Amritsar", "Ranchi", "Raipur", "Chandigarh", "Coimbatore", "Mysore", "Guwahati", "Dehradun", "Noida", "Gurgaon", "Jodhpur", "Udaipur", "Gwalior", "Jabalpur", "Aurangabad", "Nashik", "Thane", "Faridabad", "Ghaziabad", "Allahabad", "Bareilly", "Aligarh", "Moradabad", "Saharanpur", "Gorakhpur", "Jamshedpur", "Dhanbad", "Bilaspur", "Shimla", "Panaji", "Imphal", "Aizawl", "Itanagar", "Shillong", "Gangtok", "Kohima", "Agartala"
+    ];
+
+const Autocomplete = () => {
+
+    let [input, setInput] = useState('')
+    let [city, setCity] = useState('')
+
+    // setCity(input)
+
+    function handleParaClick(e){
+        setCity(e.target.innerText)
+    }
+
+    return (
+        <div className="Autocomplete">
+            <h1>Search cities of India</h1>
+            <input type="text" onChange={(e)=>{setInput(e.target.value), setCity(e.target.value)}} value={city}></input>
+            <div className="innerDiv">
+                {cities.filter(value=>value.toLowerCase().includes(input.toLowerCase())).map(value=>(
+                <p onClick={handleParaClick}>{value}</p>
+            ))}
+            </div>
+        </div>
+    )
+}
+
+export default Autocomplete
